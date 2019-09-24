f4b1f014ebda749f6f84305f2d72fdbdfa299800
okafohenrie <okafohenrie@gmail.com>
2019-09-24 08:41:51 +0100

    Added html structure in file: html

---
+++ Net worth caculator (web)/html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Networth Calculator | Landing Page</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/	bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="http://fonts.googleapis.com/css?family=roboto">
+</head>
+<body>
+	<nav class="navbar">
+		<div class="logo"></div>
+		<div class="side-links">
+			<ul>
+				<li>Log In</li>
+				<li>Sign up</li>
+				<li></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6">
+				<h3>Calculate your networth in the simplest way possible</h3>
+				<p></p>
+				<a href="#">Get Started</a>
+			</div>
+			<div class="col-md-6">
+				<img src="#">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
