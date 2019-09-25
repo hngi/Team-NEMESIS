fe1f02422a4eee4b4b6babcd39e8edbbb5450670
tobecci <ojinakatochukwu@gmail.com>
2019-09-25 11:37:21 +0100

    setup the sign in page

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_user/', views.add_user, name="add_user"),
+    # path('<str:id>/', views.details, name='details'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import User
+from django.template import loader
+from pprint import pprint
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("hello world")
+    user_list = User.objects.all()
+    template = loader.get_template('users/index.html')
+    context = {'user_list': user_list}
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("the data is " + user_list)
+
+
+def details(request, id):
+    return HttpResponse("hello number %s" % id)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        try:
+            user = User()
+            user.user_name = request.POST['name']
+            user.user_email = request.POST['email']
+            user.user_password = request.POST['password']
+            user.save()
+            return HttpResponse("USER ADDED")
+        except:
+            return HttpResponse("there was an error")
