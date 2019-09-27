4834e38b980090f12be0a3d55302799175d38fa6
MacGee20 <giftomodu20@gmail.com>
2019-09-27 12:23:18 +0100

    Neworth.html linked

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("/signUp", function(req, res){
 });
 app.get("/welcome", function(req,res){
   if(req.isAuthenticated()){
-    res.sendFile(__dirname + "/app.html");
+    res.sendFile(__dirname + "/networth.html");
   }
   else{
     res.redirect("/login");
@@ -111,3 +111,43 @@ req.login(user, function(error){
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000");
 });
+
+
+
+//Sign-in authentication
+
+function validate() {
+  var username = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  
+   
+  if (username == null || username == "") {
+      alert("Please enter the username.");
+      return false;
+  }
+  if (password == null || password == "") {
+      alert("Please enter the password.");
+      return false;
+  }
+  
+  /* 
+  
+  for(i = 0; i < user.length;i++)
+  { if(username == user[i].username && password == user[i].password) {
+      alert('Login successful : Welcome' + ' ' + username)
+     return;
+  } 
+  }
+
+  for(n = 0; n < user.length;n++)
+  {if (username !== user[n].username){
+      alert("Username does not exist");
+  return false;} 
+  
+    else if (password !== user[n].password){
+          alert("Incorrect password");
+      return false;} 
+    }
+  */
+}
+
