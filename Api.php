5659b711bcce9a0fbbb06065d80382b3869324cf
tobecci <ojinakatochukwu@gmail.com>
2019-09-29 04:13:54 +0100

    working on the signup with twitter function

--- Api.php
+++ Api.php
@@ -1,29 +1,37 @@
 <?php
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 require_once 'TwitterAPIExchange.php';
 require_once 'Utility.php';
 require_once 'twitteroauth/autoload.php';
+require_once 'config.php';
 
-$twitter_api_key = '9Kz1OWfT9ujmhgLCcwFuUFzWE';
-$twitter_api_secret_key = 'CYXJTDJUZO56bkV3fupE38Gq1yVGAF86R0zp2ZpFl6xXIux16P';
-$google_drive_api_key  = 'AIzaSyBMdOT_naOUTU9JZZfs4XudPBpy5yhVLQQ';
-$google_drive_api_secret_key = '898427961975-i77ahdt5ha8vjqstsedrmeo7tr30736o.apps.googleusercontent.com';
+// $settings = array(
+//     'oauth_access_token' => "",
+//     'oauth_access_token_secret' => "",
+//     'consumer_key' => $twitter_api_key,
+//     'consumer_secret' => $twitter_api_secret_key
+// );
 
-$settings = array(
-    'oauth_access_token' => "",
-    'oauth_access_token_secret' => "",
-    'consumer_key' => $twitter_api_key,
-    'consumer_secret' => $twitter_api_secret_key
-);
+// $username = 'tobecci';
+// $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+// $getfield = '?screen_name=' . $username . '&count=200';
+// $requestMethod = 'GET';
 
-$username = 'tobecci';
-// $url = 'https://api.twitter.com/1.1/followers/list.json';
-$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-$getfield = '?screen_name=' . $username . '&count=200';
-$requestMethod = 'GET';
+// $twitter = new TwitterAPIExchange($settings);
+// $twitter->setGetfield($getfield);
+// $twitter->buildOauth($url, $requestMethod);
+// $result = $twitter->performRequest();
+// $result = json_decode($result);
+// console_log($result);
 
-$twitter = new TwitterAPIExchange($settings);
-$twitter->setGetfield($getfield);
-$twitter->buildOauth($url, $requestMethod);
-$result = $twitter->performRequest();
-$result = json_decode($result);
-console_log($result);
+session_start();
+$connection = new TwitterOAuth($twitter_api_key, $twitter_api_secret_key);
+$request_token = $connection->oauth("oauth/request_token", array("oauth_callback" => "http://localhost:8000/twitterback.php"));
+$_SESSION['oauth_token'] = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+$url = $connection->url("oauth/authorize", array("oauth_token" => $request_token['oauth_token']));
+header('Location: ' . $url);
+// http://localhost:8000/signup.php?oauth_token=TA7sKwAAAAABAI6NAAABbXl_az4&oauth_verifier=B1NxPpHu1lkIuTgkVkg8lNJ2iPTNPkpF
+// http://localhost:8000/twitterback.php?oauth_token=cfX0XAAAAAABAI6NAAABbXmDCTk&oauth_verifier=PBWwScTDGKgf9YQ7JZHVddJrIwftvFEq
