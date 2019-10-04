2821a8fa9e9d0843d6e8d181ac6d3664b4730e7b
tobecci <ojinakatochukwu@gmail.com>
2019-10-04 15:08:14 +0100

    paused work to deploy

---
+++ login.php
@@ -0,0 +1,134 @@
+<?php
+// if (!session_id()) {
+//     session_start();
+// }
+// if (isset($_SESSION['logged_in'])) {
+//     header('Location: dashboard.php');
+// }
+// header('Location: signin.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nemesis Twitter Bot Landing Page</title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Fugaz+One|Lato:400,700|Montserrat:400,700&display=swap");
+    </style>
+</head>
+
+<body>
+    <div id="app">
+        <v-app>
+            <!-- style="background-color: rgba(29, 161, 242, 0.4);"  -->
+            <v-row>
+                <v-col cols="12" xl="6" md="6" xs="12" sm="12" lg="6" style="background-color: rgba(29, 161, 242, 0.4);">
+                    <v-container>
+                        <v-card color="transparent" flat class="mx-auto pa-3" max-width="500px" style="border-radius: 10px;">
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <img src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341483/Team%20Nemesis_twitterBot/twitter_icon_pzp2pk.svg" alt="">
+                                </v-col>
+                            </v-row>
+                            <v-row class="d-flex flex-column justify-center align-center">
+                                <div :class="{'ml-12': $vuetify.breakpoint.mdAndDown}" style="font-family: Fugaz One; font-size: 45px;">Nemesis Tweetbot</div>
+                            </v-row>
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <img src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341619/Team%20Nemesis_twitterBot/wifi_icon_mjyfgk.svg" alt="">
+                                </v-col>
+                            </v-row>
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <div>connect twitter account</div>
+                                </v-col>
+                            </v-row>
+
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <img src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341614/Team%20Nemesis_twitterBot/download_icon_njf4hm.svg" alt="">
+                                </v-col>
+                            </v-row>
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <div>Save Tweet directly from twitter to external drive</div>
+                                </v-col>
+                            </v-row>
+
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <img src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341616/Team%20Nemesis_twitterBot/file_icon_a8sddq.svg" alt="">
+                                </v-col>
+                            </v-row>
+                            <v-row class="">
+                                <v-col class="d-flex flex-row justify-center">
+                                    <div>Enjoy viewing favorite tweet on external drive at leisure</div>
+                                </v-col>
+                            </v-row>
+
+                        </v-card>
+                    </v-container>
+                </v-col>
+
+                <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12" class="d-flex flex-column justify-center align-center">
+                    <v-card class="d-flex pa-8 flex-column justify-center align-center pa-3" min-width="400px">
+                        <v-card-title class="text-center">Welcome</v-card-title>
+                        <v-container>
+                            <v-form action="hello.php">
+                                <v-text-field outlined color="blue" label="Username" placeholder="enter your username" count="10"></v-text-field>
+                                <v-text-field outlined label="Password" placeholder="enter your Password" color="blue" count="10"></v-text-field>
+                                <v-row class="d-flex flex-row justify-center">
+                                    <v-btn type="submit" color="#39a2e4" dark min-width="300px">Log in</v-btn>
+                                </v-row>
+                            </v-form>
+                            <v-row class="d-flex flex-row justify-center mt-3">
+                                <v-btn min-width="300px" color="#39a2e4" dark>Sign in with twitter</v-btn>
+                            </v-row>
+                            <v-row class="d-flex flex-row justify-center mt-3">
+                                <div class="ma-2">don't have an account?</div>
+                                <v-btn @click="gotoRegister" class="ma-2" color="#39a2e4" dark>Sign up</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-app>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+    <script>
+        var app = new Vue({
+            el: '#app',
+            vuetify: new Vuetify(),
+            data: {
+                msg: "hello",
+            },
+            methods: {
+                gotoRegister: function() {
+                    window.location.href = "register.php";
+                },
+            },
+            computed: {
+
+            },
+        });
+    </script>
+    </div>
+</body>
+
+</html>
--- signup.php
+++ signup.php
@@ -1,149 +1,35 @@
 <?php
-if (isset($_POST['submit'])) {
-    $file_content = file_get_contents('users.json');
-    $users_db = [];
-    $users_db = json_decode($file_content);
-    $new_user['name'] = strtolower($_POST['name']);
-    $new_user['email'] = strtolower($_POST['email']);
-    $new_user['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$user['name'] = $_POST['name'];
+$user['dp'] = "";
+$user['email'] = "";
+$user['text'] = $text;
+$user['username'] = $username;
+$user['logged_in'] = true;
 
-    //validation
-    $user_exists = false;
-    foreach ($users_db as $user) {
-        if ($user->name == $new_user['name']) $user_exists = true;
-        if ($user->email == $new_user['email']) $user_exists = true;
-    }
+$file_content = file_get_contents('users.json');
+$users_db = [];
+$users_db = json_decode($file_content);
+$new_user['name'] = strtolower($_POST['name']);
+$new_user['email'] = strtolower($_POST['email']);
+$new_user['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$new_user['dp'] = "";
+$new_user['text'] = "";
+$new_user['username'] = "";
+$new_user['logged_in'] = "";
 
-    if ($user_exists) {
-        header('Location: signup.php?error=user already exists');
-    } else {
-        $users_db[] = $new_user;
-        $users_db = json_encode($users_db);
-        file_put_contents('users.json', $users_db);
-        header('Location: signin.php?msg=user added successfully');
-    }
+//validation
+$user_exists = false;
+foreach ($users_db as $user) {
+    if ($user->name == $new_user['name']) $user_exists = true;
+    if ($user->email == $new_user['email']) $user_exists = true;
 }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1" />
-    <meta name="HandheldFriendly" content="true" />
-    <title>Nemesis Twitter Bot Sign Up Page</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css" />
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6 first_column">
-                <div class="content1">
-                    <img src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341483/Team%20Nemesis_twitterBot/twitter_icon_pzp2pk.svg" alt="twitter logo" />
-                    <p class="first-paragraph">Nemesis TweetBot</p>
-                    <img class="grow" src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341619/Team%20Nemesis_twitterBot/wifi_icon_mjyfgk.svg" alt="wifi icon" />
-                    <p>Connect twitter account</p>
-                    <img class="grow" src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341614/Team%20Nemesis_twitterBot/download_icon_njf4hm.svg" alt="download icon" />
-                    <p>Save Tweet directly from twitter to external drive</p>
-                    <img class="grow" src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341616/Team%20Nemesis_twitterBot/file_icon_a8sddq.svg" alt="file icon" />
-                    <p>Enjoy viewing favourite tweet on external drive at leisure</p>
-                </div>
-            </div>
-            <div class="col-sm-6 second_column">
-                <!-- <a href="signin.html"><button class="loginrdr">Login</button></a> -->
-            <form method="post" action="signup.php">
-                    <div class="form-group">
-                        <h3>Create an account</h3>
-
-                        <label for="exampleinputusername">Username</label>
-                        <input name="name" type="text" class="form-control input-box" id="exampleinputusername" aria-describedby="textHelp" required />
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleinputemail">Email</label>
-                        <input name="email" type="email" class="form-control" id="exampleinputemail" aria-describedby="textHelp" required />
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input name="password" type="password" class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <div class="form-group">
-                        <label for="examplerepeatpassword">Repeat password</label>
-                        <input type="password" class="form-control" id="examplerepeatpassword" required />
-                    </div>
-                    <div class="form-group">
-                        <div class="text-center" style="color:#39a2e4d3;">
-                            <?php
-                            echo (isset($_GET['error'])) ? '<p>' . $_GET['error'] . '</p>' : "";
-                            ?>
-                        </div>
-                    </div>
-                    <button name="submit" type="submit" class="btn btn-primary btn-block">
-                        <a href="signup.php">Signup</a>
-                    </button>
-                    <button type="submit" class="btn btn-primary btn-block">
-                        <a href="api.php">Signup with your twitter account</a>
-                    </button>
-                    <span>
-                        <p>Already have an account?</p>
-                        <button id="button" class="btn btn-primary btn-block">
-                            <a href="signin.php">Login</a>
-                        </button>
-                    </span>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <!-- <div class="row">
-        <div class="col-sm-6 first_column">
-            <div class="content1">
-                <img src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341483/Team%20Nemesis_twitterBot/twitter_icon_pzp2pk.svg" alt="twitter logo" />
-                <p class="first-paragraph">Nemesis TweetBot</p>
-                <img class="grow" src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341619/Team%20Nemesis_twitterBot/wifi_icon_mjyfgk.svg" alt="wifi icon" />
-                <p>Connect twitter account</p>
-                <img class="grow" src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341614/Team%20Nemesis_twitterBot/download_icon_njf4hm.svg" alt="download icon" />
-                <p>Save Tweet directly from twitter to external drive</p>
-                <img class="grow" src="https://res.cloudinary.com/dzwnhcpep/image/upload/v1569341616/Team%20Nemesis_twitterBot/file_icon_a8sddq.svg" alt="file icon" />
-                <p>Enjoy viewing favourite tweet on external drive at leisure</p>
-            </div>
-        </div>
-        <div class="col-sm-6 second_column">
-            <button class="loginrdr"> <a href="signin.php">Log in</a></button>
-            <form method="post" action="signup.php">
-                <div class="form-group">
-                    <h3>Create an account</h3>
-                    <label for="exampleinputusername">Username</label>
-                    <input name="name" type="text" class="form-control input-box" id="exampleinputusername" aria-describedby="textHelp" required />
-                </div>
-                <div class="form-group">
-                    <label for="exampleinputemail">Email</label>
-                    <input name="email" type="email" class="form-control input-box" id="exampleinputemail" aria-describedby="textHelp" required />
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input name="password" type="password" class="form-control" id="exampleInputPassword1" required />
-                </div>
-                <div class="form-group">
-                    <label for="examplerepeatpassword">Repeat password</label>
-                    <input type="password" class="form-control" id="examplerepeatpassword" required />
-                </div>
-                <div class="form-group">
-                    <div class="text-center" style="color:#39a2e4d3;">
-                        <?php
-                        echo (isset($_GET['error'])) ? '<p>' . $_GET['error'] . '</p>' : "";
-                        ?>
-                    </div>
-                </div>
-                <button name="submit" type="submit" class="btn btn-primary">Sign up</button>
-                <button class="btn btn-primary"><a href="api.php">Sign up with your twitter account</a></button>
-            </form>
-        </div>
-    </div> -->
-</body>
-
-</html>
+if ($user_exists) {
+    $data['error'] = "user exists";
+    echo json_encode($data);
+} else {
+    $users_db[] = $new_user;
+    $users_db = json_encode($users_db);
+    file_put_contents('users.json', $users_db);
+    $data['error'] = false;
+    echo json_encode($data);
+}
